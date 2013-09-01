8a656a058e97de1931ac4e83f03923bafb08b8ca
DUGLAR ZEMANATE <DUGLAR ZEMANATE@192.168.189.1>
2013-08-31 19:18:57 -0500

    hola primera entrega

---
+++ src/main/java/com/pasantia/entidades/prueba.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pasantia.entidades;
+
+/**
+ *
+ * @author DUGLAR ZEMANATE
+ */
+public class prueba {
+    
+}
