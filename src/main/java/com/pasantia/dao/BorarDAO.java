dad24b1343ede87ac4c8834948bee93ee4901ebc
DUGLAR ZEMANATE <DUGLAR ZEMANATE@192.168.189.1>
2013-09-02 04:24:55 -0500

    segunda entraga con, dao,daoimpl,entidades

---
+++ src/main/java/com/pasantia/dao/BorarDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pasantia.dao;
+
+/**
+ *
+ * @author DUGLAR ZEMANATE
+ */
+public interface BorarDAO {
+    
+}
